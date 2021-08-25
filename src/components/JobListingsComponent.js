41561b708893fc44318d1db88e486d29d1e6f072
yintii <keleheart1@gmail.com>
2021-08-25 07:33:33 -0700

    set up the basis of the project buy initializing it with some basic lisitings

---
+++ src/components/JobListingsComponent.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Card, CardHeader, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class JobListings extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedJob: null
+        };
+    }
+    render(){
+        const jobListings = this.props.jobs.map(job => {
+            return(
+                <div key={job.id} className="col m-1">
+                    <Card>
+                        <CardBody>
+                            <CardHeader dark className="bg-dark text-white">
+                                {job.company}
+                            </CardHeader>
+                            <CardText>
+                                {job.position}
+                                <br/>
+                                {job.pay}
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                    
+                </div>
+            );
+        });
+
+        return(
+            <div className="container">
+                <div className="row">
+                    {jobListings}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default JobListings;
---
+++ src/shared/Jobs.js
@@ -0,0 +1,20 @@
+export const JOBS = [
+    {
+        id:0,
+        company: 'Microsoft',
+        position: 'Data Scientist',
+        pay: '$70/hr'
+    },
+    {
+        id:1,
+        company: 'Apple',
+        position: 'Senior C Developer',
+        pay: '$110,000 annual salary'
+    },
+    {
+        id:2,
+        company: 'Intuit',
+        position: 'Systems Architect',
+        pay: '120k / year'
+    }
+]
